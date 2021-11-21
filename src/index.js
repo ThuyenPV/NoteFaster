774ae32847551fcd694f106cf9c621cce44be1c5
Entonylab <thuyen.pham@enlabsoftware.com>
2021-11-21 15:23:53 +0700

    Using bootstrap

--- src/index.js
+++ src/index.js
@@ -9,12 +9,23 @@ app.use(express.static(path.join(__dirname, "public")));
 // HTTP Logger
 app.use(morgan("combined"));
 
+// Template engine
 app.engine("hbs", engine({ extname: ".hbs", defaultLayout: "main" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
+// Routing
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.get("/news", (req, res) => {
+  console.log(req.query.q);
+  res.render("news");
 });
 
 app.listen(3000);
